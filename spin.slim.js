f52deadf11f6c906744e13030a6754a036dce1c9
Jonas Persson <11533019+jop-io@users.noreply.github.com>
2018-09-11 12:42:14 +0200

    Uppdaterade regexp

--- spin.slim.js
+++ spin.slim.js
@@ -1,7 +1,7 @@
 function SPIN(input)
 {
     // Check if input ID is malformed
-    if (!/^([0-9]{6}|[0-9]{8})[\-\+]{0,1}[0-9]{4}$/.test(input))
+    if (!/^([0-9]{6}|[0-9]{8})[\-\+ ]{0,1}[0-9]{4}$/.test(input))
     {
         return false;
     }   
